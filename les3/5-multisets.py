35bd2225ed0dd25911f310e7c1db449d4fba3588
Dieterdemuynck <dieter.demuynck30@gmail.com>
2020-10-15 17:20:21 +0200

    Fixed 5

--- les3/5-multisets.py
+++ les3/5-multisets.py
@@ -1,6 +1,38 @@
-# I'm FUCKIN UP and I can't get shit to work.
-# Gimme a second, fixing this entire code.
+# Fixed it. Finally.
+# Not my finest moment.
 
+def union(set1, set2):
+    uniset = dict(set1)
+
+    for key, value in set2.items():
+        if key in set1:
+            uniset[key] += value
+        else:
+            uniset[key] = value
+
+    return uniset
+
+
+def intersection(set1, set2):
+    interset = {}
+
+    for key, value in set1.items():
+        if key in set2:
+            interset[key] = min(set2[key], value)
+
+    return interset
+
+
+def difference_set(set1, set2):
+    diffset = dict(set1)
+
+    for key, value in set2.items():
+        if key in set1 and set1[key] > value:
+            diffset[key] -= value
+        elif key in set1:
+            diffset.pop(key)
+
+    return diffset
 
 
 # Testing the functions:
@@ -10,5 +42,5 @@ print("set1 U set2 =", union(set1, set2))
 print("set2 U set1 =", union(set2, set1))
 print("set1 /\\ set2 =", intersection(set1, set2))  # double backslash needed to negate what a backslash would do
 print("set2 /\\ set1 =", intersection(set2, set1))
-print("set1 - set2 =", difference(set1, set2))
-print("set2 - set1 =", difference(set2, set1))
+print("set1 - set2 =", difference_set(set1, set2))
+print("set2 - set1 =", difference_set(set2, set1))
