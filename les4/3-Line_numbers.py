e493bc71bc794ade9d99e7581fef0ced5e55b1d1
Dieterdemuynck <dieter.demuynck30@gmail.com>
2020-10-22 17:14:44 +0200

    Updated 3 and added files

--- les4/3-Line_numbers.py
+++ les4/3-Line_numbers.py
@@ -1,4 +1,4 @@
-def add_line_number(text):
+def add_line_numbers_txt(text):
     text = text.split("\n")
     i = 1
     text_w_numbers = []
@@ -10,13 +10,27 @@ def add_line_number(text):
     return "\n".join(text_w_numbers)
 
 
-def main():
-    # inputfile = input("Name of input:\n> ")
-    # outputfile = input("Name of output:\n> ")
-    text = "Hello!\nI am David.\nI am a student, and I am currently studying.\n"
-    text += "Studying stands for \"student dying\". Please send help."
+def add_line_numbers(inputfile, outputfile):
+    with open(inputfile) as file:
+        text = file.read()
+    file.close()
 
-    print(add_line_number(text))
+    text = add_line_numbers_txt(text)
+
+    with open(outputfile, "w") as file:
+        file.write(text)
+    file.close()
+
+
+def main():
+    inputfile = input("Name of input:\n> ")
+    outputfile = input("Name of output:\n> ")
+    # text = "Hello!\nI am David.\nI am a student, and I am currently studying.\n"
+    # text += "Studying stands for \"student dying\". Please send help."
+
+    add_line_numbers(inputfile, outputfile)
+
+    print("Line numbers added.")
 
 
 main()
