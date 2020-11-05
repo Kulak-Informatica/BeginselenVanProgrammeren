8aba79a81a5b7746363c7762a05eac863c91cda5
Dieterdemuynck <dieter.demuynck30@gmail.com>
2020-11-05 16:54:37 +0100

    4 - Sum of digits recursive

--- les6/4-Sum_of_digits_REC.py
+++ les6/4-Sum_of_digits_REC.py
@@ -1,1 +1,12 @@
 def sum_of_digits_rec(num):
+    num = str(num)  # string of string won't give error
+    if len(num) == 1:
+        return int(num)
+    return int(num[0]) + sum_of_digits_rec(num[1:])
+
+
+def main():
+    print(sum_of_digits_rec(55555))
+
+
+main()
