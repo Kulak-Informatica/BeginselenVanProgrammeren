0cb3ae80545f382182e5275fab22a86cbb66c824
Dieterdemuynck <dieter.demuynck30@gmail.com>
2020-11-05 16:51:13 +0100

    3 - Updated comment

--- les6/3-Palindrome_REC.py
+++ les6/3-Palindrome_REC.py
@@ -1,7 +1,8 @@
 def palindrome_rec(string):
     if len(string) <= 1:
         return True
-    elif string[0] != string[-1]:
+    elif string[0] != string[-1]:  # if we write "not" this statement in the return, it'll be a bit more efficient, \
+        # and this if statement won't be necessary
         return False
     return True and palindrome_rec(string[1:-1])
 
---
+++ les6/4-Sum_of_digits_REC.py
@@ -0,0 +1,1 @@
+def sum_of_digits_rec(num):
